5ee9449cf9a68962d5ca2b5d3a7e2561578e5734
shist2023 <shist2023@gmail.com>
2024-04-19 11:07:31 +0800

    change bcrypt

--- auth.ts
+++ auth.ts
@@ -1,25 +1,28 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
-import Credentials from "next-auth/providers/credentials"
-import bcrypt from 'bcrypt';
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
-import type { User } from '@/lib/definitions';
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import type { User } from "@/lib/definitions";
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0];
   } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
   }
 }
+
+var bcrypt = require('bcryptjs');
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/login",
   },
+  // theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   secret: process.env.AUTH_SECRET,
   providers: [
     Github,
@@ -27,8 +30,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-
+          .safeParse(credentials);        
+        
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
@@ -44,6 +47,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     authorized({ request: { nextUrl }, auth }) {
       console.log(`authorized:${nextUrl} `);
+      //credentials:http://localhost:3000/login?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fstudio
+      //github:
       if (nextUrl.pathname.startsWith("/studio")) return !!auth;
       return true;
     },
