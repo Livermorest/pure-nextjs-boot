66026fe8611c144ade5d48b253ffb55a4661ad0d
Livermorest <shist2023@gmail.com>
2024-05-01 22:44:40 +0800

    1

--- components/login/login-form.tsx
+++ components/login/login-form.tsx
@@ -112,13 +112,13 @@ export default function LoginForm() {
             <Button onClick={()=>signIn('google')}
               className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 bpp"
             >
-              {google()}
+              {googleLogo()}
               <span className="text-sm font-semibold leading-6">Google</span>
             </Button>
             <Button onClick={()=>signIn('github')}
               className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 bpp"
             >
-              {github()}
+              {githubLogo()}
               <span className="text-sm font-semibold leading-6">GitHub</span>
             </Button>
           </div>
